eb175fd7e9f608893f05d88011794ca7267e9d77
Abdelatif Bensahnoun <latifben26@gmail.com>
2024-04-11 02:41:53 +0100

    fixing background color conflict on selecting options

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,24 +1,27 @@
 function Options({ question, answers, dispatch, active }) {
   const hasAnswered = active !== null;
+
   return (
     <div className="grid sm:grid-cols-2 gap-4 mt-4 sm:mt-16 place-items-center">
       {answers?.map((answer, index) => (
         <button
-          className={`p-2 rounded-full bg-transparent border-2 w-10/12 border-black/60 ${
-            index === active
-              ? answer !== question.correctAnswer
-                ? "bg-red-400"
-                : "bg-green-400"
+          className={`p-2 rounded-full  border-2 w-10/12 border-black/60 ${
+            active !== null && index === active
+              ? answer === question.correctAnswer
+                ? "bg-green-300"
+                : "bg-red-400"
               : ""
           }`}
           key={answer}
           disabled={hasAnswered}
           onClick={() => {
-            dispatch({
-              type: "newAnswer",
-              payload: index,
-              score: answer,
-            });
+            if (!hasAnswered) {
+              dispatch({
+                type: "newAnswer",
+                payload: index,
+                score: answer,
+              });
+            }
           }}
         >
           {answer}
